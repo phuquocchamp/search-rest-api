fda617288c0b5ab23aa71a8203b819cbf6237491
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-02-26 20:00:51 +0700

    create product entity

---
+++ src/main/java/com/example/searchrestapi/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.searchrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private boolean active;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime dateUpdated;
+}
